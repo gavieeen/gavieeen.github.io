d517210d0d36567dadb8b18f2eb0054ff155ccf6
gavieeen <gavinebenezer@gmail.com>
2024-07-18 12:02:03 -0500

    change to HashRouter from BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import React from 'react'
-import { Route, BrowserRouter as Router, Routes, useLocation } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import { Home, About, Projects, Contact, Credits } from './pages';
 import Navbar from "./components/navbar";
 import Loader from './components/Loader';
